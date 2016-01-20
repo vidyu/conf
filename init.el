72fffa7aa49f256e7ee50bf1b43e796d36140325
Vidyu Krastev <v.krastev@yourrate.com>
2016-01-20 10:00:48 +0100

    Added flycheck js checkers

--- init.el
+++ init.el
@@ -9,6 +9,13 @@
    (or (buffer-file-name) load-file-name))
   "Base path for customised Emacs configuration.")
 
+; Syntactic sugar for eval-after-load dance.
+;; https://github.com/purcell/emacs.d/blob/aa789c9745b13612c4fea6e638d81d8ebbfecdf8/init-utils.el#L1-L5
+(defmacro after-load (feature &rest body)
+  "After FEATURE is loaded, evaluate BODY."
+  (declare (indent defun))
+  `(eval-after-load ,feature
+     '(progn ,@body)))
 
 (require 'cask)
 (cask-initialize)
--- init/setup-js2.el
+++ init/setup-js2.el
@@ -10,5 +10,9 @@
 (add-hook 'js2-mode-hook 'whitespace-mode)
 (add-hook 'js2-mode-hook 'whitespace-cleanup-mode)
 
+(after-load 'js2-mode
+            (after-load 'flycheck
+                        (add-to-list 'exec-path (concat dotfiles-dir "node_modules/.bin/"))))
+
 (provide 'setup-js2)
 ;;; setup-js2.el ends here
