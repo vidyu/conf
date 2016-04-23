a620223f27722b3b6a3e7da0822c12b006c500a9
Vidyu Krastev <v.krastev@yourrate.com>
2016-04-23 08:20:43 +0200

    change ediff faces

--- custom.el
+++ custom.el
@@ -101,7 +101,13 @@
  ;; If there is more than one, they won't work right.
  '(default ((t (:height 100 :family "Inconsolata LGC"))))
  '(ediff-current-diff-C ((t (:background "saddle brown"))))
+ '(ediff-even-diff-A ((t (:background "gray32"))))
+ '(ediff-even-diff-B ((t (:background "gray32"))))
+ '(ediff-even-diff-C ((t (:background "gray32"))))
  '(ediff-fine-diff-B ((t (:background "dark green"))))
+ '(ediff-odd-diff-A ((t (:background "gray24"))))
+ '(ediff-odd-diff-B ((t (:background "gray24"))))
+ '(ediff-odd-diff-C ((t (:background "gray24"))))
  '(hl-line ((t (:weight bold))))
  '(show-paren-match ((t (:underline t))))
  '(whitespace-indentation ((t (:foreground "dark gray"))))
