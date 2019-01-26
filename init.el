18bbce4d28c399b1400a1c1e624bfe0ea296284f
Vidyu Krastev <dev.vidyu@gmail.com>
2019-01-26 11:16:33 +0100

    add d layer

--- init.el
+++ init.el
@@ -33,7 +33,8 @@ This function should only modify configuration layer settings."
 
    ;; List of configuration layers to load.
    dotspacemacs-configuration-layers
-   '(typescript
+   '(d
+     typescript
      (node :variables
            node-add-modules-path t)
      react
